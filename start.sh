abe6d5e2853e997d2b677479a09aa24eb3956ddb
rol4nd <github@rowoco.de>
2016-03-10 13:55:57 +0100

    Flexible Path

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "Sie müssen sich als 'root' anmelden!" 1>&2
+   exit 1
+fi
+
+# This file should be the Start of all function
+
+# Get the current mainfolder
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  fi
+done
+echo "SOURCE is '$SOURCE'"
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+if [ "$DIR" != "$RDIR" ]; then
+  echo "DIR '$RDIR' resolves to '$DIR'"
+fi
+echo "DIR is '$DIR'"
