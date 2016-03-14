59306e1efcfa4ae54e3c63934451dd0ee7027e62
rol4nd <github@rowoco.de>
2016-03-14 17:15:18 +0100

    Flexible Path

--- install/installbash.sh
+++ install/installbash.sh
@@ -3,23 +3,9 @@
 rm /etc/bash.bashrc
 
 OLD="menupath"
-NEW="$2"
-echo $NEW
-DPATH="$2/bashrc/bash.bashrc"
-BPATH="/etc"
-TFILE="/tmp/out.tmp.$$"
+NEW=$2
 
-[ ! -d $BPATH ] && mkdir -p $BPATH || :
-for f in $DPATH
-do
-  if [ -f $f -a -r $f ]; then
-    /bin/cp -f $f $BPATH
-   sed "s/$OLD/$NEW/g" "$f" > $TFILE && mv $TFILE "$f"
-  else
-   echo "Error: Cannot read $f"
-  fi
-done
-/bin/rm $TFILE
+cp $2/bashrc/bash.bashrc /etc/bash.bashrc
+rpl $OLD $NEW /etc/bash.bashrc
 
-
-#bash $1
+bash $1
