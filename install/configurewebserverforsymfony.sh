f54b75c300d537e2637cf31f58584f29a2de2c36
rol4nd <github@rowoco.de>
2016-03-15 16:06:10 +0100

    Flexible Path

--- install/configurewebserverforsymfony.sh
+++ install/configurewebserverforsymfony.sh
@@ -2,12 +2,12 @@
 
 a2enmod rewrite
 
-cp $1/symfony/symfony.conf /etc/apache2/site-available/symfony.conf
+cp $1/symfony/symfony.conf /etc/apache2/sites-available/symfony.conf
 ln -s /etc/apache2/sites-available/symfony.conf /etc/apache2/sites-enabled/symfony.conf
-cp /etc/apache2/sites-available/default.conf /etc/apache2/sites-available/default.conf.old
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
 
-rm /etc/apache2/sites-available/default.conf
-rm /etc/apache2/sites-enabled/default.conf
+rm /etc/apache2/sites-available/000-default.conf
+rm /etc/apache2/sites-enabled/000-default.conf
 
 service apache2 restart
 
