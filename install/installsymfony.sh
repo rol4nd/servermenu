7e79c6a5d8818d88ad98020923c2fcb0da9e01ea
rol4nd <github@rowoco.de>
2016-03-15 14:20:54 +0100

    Flexible Path

--- install/installsymfony.sh
+++ install/installsymfony.sh
@@ -4,4 +4,4 @@ curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 chmod a+x /usr/local/bin/symfony
 
 curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
