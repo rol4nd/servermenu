91ff641e8912bcff02cfee9a8c217de55b23e47b
rol4nd <github@rowoco.de>
2016-03-16 08:53:01 +0100

    Final Release

--- symfony/createnewsymfonyproject.sh
+++ symfony/createnewsymfonyproject.sh
@@ -7,6 +7,10 @@ symfony new "$defaultfolder/$projectname"
 
 cp $2/symfony/symfonymenu.sh $defaultfolder/$projectname/symfonymenu.sh
 
-chown www-data:www-data $defaultfolder/$projectname/* -R
+chown www-data:www-data $defaultfolder/* -R
+
+useradd -o -u 33 -g www-data -d /var/www/symfony -s /bin/bash -c ftpsymfony
+echo "ftpsymfony:F1leupl0ad" | chpasswd
+
 
 bash $1
