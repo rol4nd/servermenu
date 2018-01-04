7ed530979b2b8f0e9ee45096cc52761619e38fd9
rol4nd <github@rowoco.de>
2018-01-04 21:16:47 +0100

    Install MariaDB

--- install/installmariadb.sh
+++ install/installmariadb.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get install -y mariadb-server
+apt-get install software-properties-common dirmngr
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.hosteurope.de/mirror/mariadb.org/repo/10.2/debian stretch main'
 
+apt-get update
+apt-get install mariadb-server
 bash $1
