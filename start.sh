6f2d4cdefdeac7462ba5aae011ccb163950502ab
rol4nd <github@rowoco.de>
2018-01-11 23:46:47 +0100

    Add Todo

--- start.sh
+++ start.sh
@@ -46,14 +46,23 @@ BACKTITLE="Serveradministration/-installation"
 TITLE="Servermenu"
 MENU="Please choose a option:"
 
+
+
+#Servermenu
 OPTIONS=(1 "Update Server"
-         2 "Install Bashrc"
-         3 "Install Webserver (mariaDB Client, Apache2, PHP7.0, proFTPD)"
-         4 "Install MariaDB Server"
-         11 "Prepare for using Symfony"
-         12 "Install new Symfony-Project"
-         14 "Configure Webserver for Symfonyproject"
          99 "Reboot Server")
+
+
+
+
+#OPTIONS=(1 "Update Server"
+#         2 "Install Bashrc"
+#         3 "Install Webserver (mariaDB Client, Apache2, PHP7.0, proFTPD)"
+#         4 "Install MariaDB Server"
+#         11 "Prepare for using Symfony"
+#         12 "Install new Symfony-Project"
+#         14 "Configure Webserver for Symfonyproject"
+#         99 "Reboot Server")
 
 showdialog()
 {
@@ -71,12 +80,6 @@ showdialog
 clear
 case $CHOICE in
     1) bash $DIR/update/update.sh $Startfile $DIR;;
-	2) bash $DIR/install/installbash.sh $Startfile $DIR;;
-	3) bash $DIR/install/installwebserver.sh $Startfile $DIR;;
-	4) bash $DIR/install/installmariadb.sh $Startfile $DIR;;
-	11) bash $DIR/install/installsymfony.sh $Startfile $DIR;;
-	12) bash $DIR/symfony/createnewsymfonyproject.sh $Startfile $DIR;;
-	14) bash $DIR/install/configurewebserverforsymfony.sh $Startfile $DIR;;
 	99) reboot;;
 esac
 
