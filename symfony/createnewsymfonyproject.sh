4b5b48c6a3394fe501b99342914138a398a95e7a
rol4nd <github@rowoco.de>
2018-01-12 00:23:26 +0100

    Add Todo

--- symfony/createnewsymfonyproject.sh
+++ symfony/createnewsymfonyproject.sh
@@ -7,4 +7,29 @@ composer create-project symfony/skeleton "$defaultfolder/$projectname"
 
 chown www-data:www-data $defaultfolder/* -R
 
+#Create Database and User
+# create random password
+PASSWDDB="Te3XtcH"
+
+# replace "-" with "_" for database username
+MAINDB="symfony"
+
+# If /root/.my.cnf exists then it won't ask for root password
+if [ -f /root/.my.cnf ]; then
+
+    mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+# If /root/.my.cnf doesn't exist then it'll ask for root password
+else
+    echo "Please enter root user MySQL password!"
+    read $PASSWDDB
+    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+fi
+
 bash $1
