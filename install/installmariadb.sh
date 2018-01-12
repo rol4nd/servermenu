d07a1ae0be5933138dfce32e05ab191362a0b5a1
rol4nd <github@rowoco.de>
2018-01-12 08:25:22 +0100

    Add per Default git/subversion

--- install/installmariadb.sh
+++ install/installmariadb.sh
@@ -2,8 +2,8 @@
 
 PASSWORD="Te3XtcH"
 export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<< "mariadb-server mariadb-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mariadb-server mariadb-server/root_password_again password $PASSWORD"
+debconf-set-selections <<< "mariadb-server mariadb-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mariadb-server mariadb-server/root_password_again password $PASSWORD"
 
 apt-get install -y mariadb-server python-mysqldb
 
