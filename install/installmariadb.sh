908e76b0c5c31d90919e8d8d920740725841ce7c
rol4nd <github@rowoco.de>
2018-01-15 19:52:45 +0100

    Add basic requirements for Symfony 4

--- install/installmariadb.sh
+++ install/installmariadb.sh
@@ -5,8 +5,6 @@ PASSWORD="Te3XtcH"
 #debconf-set-selections <<< "mariadb-server mariadb-server/root_password password $PASSWORD"
 #debconf-set-selections <<< "mariadb-server mariadb-server/root_password_again password $PASSWORD"
 
-apt-get install -y mariadb-server python-mysqldb
-
-#mysql -u root -e "SET PASSWORD FOR root@'localhost' = PASSWORD(‘$PASSWORD’);"
+apt-get install mariadb-server python-mysqldb
 
 bash $1
