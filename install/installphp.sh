ed5905f5c6c2fbfc843877177e6f5a62ef7841f9
rol4nd <github@rowoco.de>
2018-02-24 23:39:32 +0100

    ProFTPd als eigener Menüpunkt

--- install/installphp.sh
+++ install/installphp.sh
@@ -36,7 +36,7 @@ then
 	sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
 	sudo apt-get update
-	sudo apt-get install php7.2-cli php7.2-xml
+	sudo apt-get -y -gg install php7.2-cli php7.2-xml
 	checkInstallApache
 else
 	echo "no value"
