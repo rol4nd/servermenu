0155b7583f13e991cfbdee9c7dc9cca731a5452b
rol4nd <github@rowoco.de>
2018-02-24 23:32:08 +0100

    ProFTPd als eigener Menüpunkt

--- install/installphp.sh
+++ install/installphp.sh
@@ -5,7 +5,6 @@ echo "Install PHP"
 #mariaDB-Client
 apt-get install -y -qq mariadb-client
 
-apt-get install -y -gg apt-transport-https lsb-release ca-certificates
 apt-get install -y -qq apt-transport-https
 
 checkInstallApache()
@@ -33,10 +32,11 @@ then
 elif [ $3 == "7.2" ]
 then
 	#PHP 7.2
-	wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+	sudo apt-get install apt-transport-https lsb-release ca-certificates
+	sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-	apt-get update
-	apt-get install php7.2-cli php7.2-xml
+	sudo apt-get update
+	sudo apt-get install php7.2-cli php7.2-xml
 	checkInstallApache
 else
 	echo "no value"
