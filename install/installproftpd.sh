1d50ca2ffe731165d4a66b9fa99679cb857c1e2f
rol4nd <github@rowoco.de>
2018-02-24 23:02:18 +0100

    ProFTPd als eigener Menüpunkt

--- install/installproftpd.sh
+++ install/installproftpd.sh
@@ -3,16 +3,16 @@
 echo "Install proFtpd"
 
 #proftpd
-#apt-get install -y -qq proftpd
-#useradd -o ftpuser -u 33 -g www-data -d /var/www/symfony -s /bin/bash
-#echo "ftpuser:F1leupl0ad" | chpasswd
+apt-get install -y -qq proftpd
+useradd -o ftpuser -u 33 -g www-data -d /var/www/symfony -s /bin/bash
+echo "ftpuser:F1leupl0ad" | chpasswd
 
 # DefaultRoot
 OLD="# DefaultRoot"
 NEW="DefaultRoot"
 
-#rpl "$OLD" "$NEW" /etc/proftpd/proftpd.conf
+rpl "$OLD" "$NEW" /etc/proftpd/proftpd.conf
 
-#service proftpd restart
+service proftpd restart
 
-#bash $1
+bash $1
