d6fa2999cd09881c880e90a230211f49e78a2e69
rol4nd <github@rowoco.de>
2019-05-19 00:42:29 +0200

    Add PHP 7.3

--- install/installphp.sh
+++ install/installphp.sh
@@ -49,7 +49,7 @@ then
 	#PHP 7.3
 	sudo apt -y install lsb-release apt-transport-https ca-certificates
     sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-    echo “deb https://packages.sury.org/php/ $(lsb_release -sc) main” | sudo tee /etc/apt/sources.list.d/php7.3.list
+    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
 
 	sudo apt-get update
 	apt-get -y install php7.3
