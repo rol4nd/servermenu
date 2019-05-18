15613fccba5c3e01ed68977018e42563a101384f
rol4nd <github@rowoco.de>
2019-05-19 00:59:03 +0200

    Add PHP 7.3

--- install/installsymfony.sh
+++ install/installsymfony.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+    if [ "$(whoami)" != "root" ]; then
+        SUDO=sudo
+    fi
 
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+
+
+    ${SUDO} mkdir -p /usr/local/bin
+    ${SUDO} curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+    ${SUDO} chmod a+x /usr/local/bin/symfony
 bash $1
