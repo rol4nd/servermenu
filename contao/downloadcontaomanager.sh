094d7ab024f92a33ca6ba40be2020cc7881f6233
Wollenschläger Roland (MaierKorduletsch) <rwollenschlaeger@maierkorduletsch.de>
2020-10-14 13:06:37 +0200

    contao

--- contao/downloadcontaomanager.sh
+++ contao/downloadcontaomanager.sh
@@ -3,7 +3,8 @@
 defaultfolder="/var/www"
 
 cd "$defaultfolder"
-mkdir "$defaultfolder/contao/web"
+mkdir -p /var/www/contao
+mkdir -p /var/www/contao/web
 cd "$defaultfolder/contao/web"
 wget https://download.contao.org/contao-manager/stable/contao-manager.phar
 mv contao-manager.phar contao-manager.phar.php
