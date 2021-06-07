bac27008ccfa69941b8dcb12159a60c81d577668
rol4nd <github@rowoco.de>
2021-06-07 16:39:57 +0200

    Add PHP8.0

--- install/installwebserver.sh
+++ install/installwebserver.sh
@@ -13,10 +13,11 @@ echo "Install PHP"
 sudo apt install -y mariadb-client
 sudo apt install -y apt-transport-https
 
-sudo apt install -y lsb-release apt-transport-https ca-certificates software-properties-common
+sudo apt install -y gnupg2 ca-certificates apt-transport-https software-properties-common
 
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
 wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ buster main" | sudo tee /etc/apt/sources.list.d/php.list
+
 
 sudo apt update
 
@@ -25,6 +26,8 @@ sudo apt install -y php8.0-cli php8.0-fpm php8.0-json php8.0-pdo php8.0-mysql ph
 sudo apt install -y php8.0-mbstring php8.0-curl php8.0-xml php8.0-bcmath php8.0-json php8.0-curl php8.0-intl
 
 sudo apt install -y php-xdebug php-apcu
+
+sudo apt install -y apache2 libapache2-mod-php8.0 
 
 sudo service apache2 restart
 
